c35d5e4622e0547fcdf6e55f88b0ddab25903482
Ankitachaubey01 <61126648+Ankitachaubey01@users.noreply.github.com>
2026-02-06 15:17:27 +0530

    Replace hardcoded SSH key name with variable

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -7,7 +7,7 @@ module "EKS_Cluster" {
   vpc_cidr              = var.vpc_cidr
   availability_zones    = var.availability_zones
   eks_cluster_name      = var.eks_cluster_name
-  ssh_key_name = "ekskey"
+  ssh_key_name          = var.ssh_key_name
   node_group_name       = var.node_group_name
   instance_types        = var.instance_types
   desired_size          = var.desired_size
