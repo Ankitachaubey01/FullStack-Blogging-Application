a7caffcc3c6e590db12b5831f1055c0840bc16cd
Ankitachaubey01 <61126648+Ankitachaubey01@users.noreply.github.com>
2026-02-06 14:13:13 +0530

    Remove tf_locks DynamoDB table resource
    Removed DynamoDB table resource definition for tf_locks.

--- terraform/modules/main.tf
+++ terraform/modules/main.tf
@@ -171,11 +171,6 @@ resource "aws_s3_bucket" "tf_state" {
   bucket = var.tf_state_bucket
 }
 
-resource "dynamodb_table" "tf_locks" {
-  name         = var.tf_lock_table
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
   attribute {
     name = "LockID"
     type = "S"
